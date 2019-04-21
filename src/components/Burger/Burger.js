c045f9ae265eaebf0c264b81f4438e560bfb9ecd
Mahesh Kumar Kumawat <pot30270@posten.local>
2019-04-22 01:00:24 +0200

    ingredinets validation messaged added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,17 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredient)
+    let transformedIngredients = Object.keys(props.ingredient)
         .map((igKey)=> {
             return [...Array(props.ingredient[igKey])].map((_,i)=>{
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
-        })
+        }).reduce((arr,el) =>{
+            return arr.concat(el);
+        },[]);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Pleast start adding ingredients </p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
