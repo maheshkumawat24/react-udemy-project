6b8e9c33fb2ecbddf7f9bdf18b9cd044ab230e8c
Mahesh Kumar Kumawat <pot30270@posten.local>
2019-04-22 00:16:18 +0200

    implemented burger component and made BurgerIngredient in action

--- src/container/BurgerBuilder.js
+++ src/container/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Aux from '../hoc/Auxx';
+import Burger from '../components/Burger/Burger';
 
 export class BurgerBuilder extends Component {
     render() {
         return (
            <Aux>
-               <div>Burger</div>
+               <Burger></Burger>
                <div>Build controls</div>
            </Aux>
         )
