62310bedf7cd0d95dd2b88b986ac9dacc0169839
Mahesh Kumar Kumawat <pot30270@posten.local>
2019-05-27 22:42:27 +0200

    backdrop and animation added in sidedrawer class

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxx';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = (props) => (
-    <Aux>
-        <Toolbar></Toolbar>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
 
-export default layout;
+class Layout extends React.Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar></Toolbar>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+export default Layout;
